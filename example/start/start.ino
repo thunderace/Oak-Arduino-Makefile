f60c7d5c5a60d82fd7597d57621ea1e50a4e3901
thunder <thunder@debianserver.thunderace.fr>
2016-02-22 21:35:46 +0100

    work fine now

--- example/start/start.ino
+++ example/start/start.ino
@@ -1,15 +1,18 @@
 #include <Arduino.h>
 #include "myLib.h"
 
+int temperature = 1;
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(1, OUTPUT); //LED on Oak
+  Particle.variable("temperature", temperature);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   digitalWrite(1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  temperature++;
   delay(2000);               // wait for a second
   digitalWrite(1, LOW);    // turn the LED off by making the voltage LOW
-  delay(2000);               // wait for a second
+  delay(2000);  
 }
